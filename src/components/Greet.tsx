d269906bd3344ad9947b27d3439982858178e5ba
Emdadul_Islam <emdadulislam439@gmail.com>
2023-06-03 12:13:28 +0600

    experimenting: just used alternatives to see if the code works

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,8 +1,10 @@
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
+
 type GreetProps = {
-  name: string;
+  name: string | number;
 };
 
-export const Greet = (props: GreetProps) => {
+export const Greet = (props: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) => {
   return (
     <div>
       <h2>Welcome {props.name}! You have 10 unread messages</h2>
